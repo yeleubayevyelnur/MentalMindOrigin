98b1297830a8244616eeb7915005bdd057f1022d
Yeldos Yeleubayev <e.eleubaev@dodopizza.com>
2020-10-25 14:43:22 +0600

    customize player

--- app/src/main/java/kz/mentalmind/ui/player/PlayerActivity.kt
+++ app/src/main/java/kz/mentalmind/ui/player/PlayerActivity.kt
@@ -1,14 +1,12 @@
 package kz.mentalmind.ui.player
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatTextView
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.ui.PlayerView
-import kotlinx.android.synthetic.main.activity_player.*
+import com.google.android.exoplayer2.ui.PlayerControlView
 import kz.mentalmind.R
 import kz.mentalmind.domain.dto.MeditationDto
 import kz.mentalmind.utils.Constants.MEDITATION
@@ -16,41 +14,43 @@ import kz.mentalmind.utils.Constants.MEDITATION
 class PlayerActivity : AppCompatActivity() {
     private var exoPlayer: SimpleExoPlayer? = null
     private var soundsPlayer: SimpleExoPlayer? = null
-    private var playerView: PlayerView? = null
+    private var playerView: PlayerControlView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
         val meditation: MeditationDto? = intent?.getParcelableExtra(MEDITATION)
         meditation?.let {
+            findViewById<AppCompatTextView>(R.id.title).text = it.name
+            findViewById<AppCompatTextView>(R.id.description).text = it.description
             initializePlayer(it)
         }
-        sea?.setOnClickListener {
-            soundsPlayer?.setMediaItem(MediaItem.fromUri("file:///android_asset/0eb24fcad1bffed.mp3"))
-        }
-        birds.setOnClickListener {
-            soundsPlayer?.setMediaItem(MediaItem.fromUri("file:///android_asset/544537.mp3"))
-        }
-        italy?.setOnClickListener {
-            soundsPlayer?.setMediaItem(MediaItem.fromUri("file:///android_asset/e55bd2beca1a2a1.mp3"))
-        }
-
-        volumeBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                soundsPlayer?.volume = when (p1) {
-                    0 -> 0f
-                    100 -> 1f
-                    else -> p1.toFloat() / 100.toFloat()
-                }
-                Log.d("yel", "sounds volume ${soundsPlayer?.volume}")
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
-            }
-        })
+//        sea?.setOnClickListener {
+//            soundsPlayer?.setMediaItem(MediaItem.fromUri("file:///android_asset/0eb24fcad1bffed.mp3"))
+//        }
+//        birds.setOnClickListener {
+//            soundsPlayer?.setMediaItem(MediaItem.fromUri("file:///android_asset/544537.mp3"))
+//        }
+//        italy?.setOnClickListener {
+//            soundsPlayer?.setMediaItem(MediaItem.fromUri("file:///android_asset/e55bd2beca1a2a1.mp3"))
+//        }
+//
+//        volumeBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+//            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+//                soundsPlayer?.volume = when (p1) {
+//                    0 -> 0f
+//                    100 -> 1f
+//                    else -> p1.toFloat() / 100.toFloat()
+//                }
+//                Log.d("yel", "sounds volume ${soundsPlayer?.volume}")
+//            }
+//
+//            override fun onStartTrackingTouch(p0: SeekBar?) {
+//            }
+//
+//            override fun onStopTrackingTouch(p0: SeekBar?) {
+//            }
+//        })
     }
 
     override fun onDestroy() {
@@ -67,6 +67,7 @@ class PlayerActivity : AppCompatActivity() {
             prepare()
         }
         playerView = findViewById(R.id.exo_player_view)
+        playerView?.setBackgroundColor(getColor(android.R.color.white))
         playerView?.player = exoPlayer
 
         soundsPlayer = SimpleExoPlayer.Builder(this)
