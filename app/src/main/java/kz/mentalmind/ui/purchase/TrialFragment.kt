ed82a0cd4b5737222c7bdd65ae171aa68a34db7a
00049508 <7>
2020-11-22 20:24:29 +0600

    tariff items are added

--- app/src/main/java/kz/mentalmind/ui/purchase/TrialFragment.kt
+++ app/src/main/java/kz/mentalmind/ui/purchase/TrialFragment.kt
@@ -1,9 +1,12 @@
 package kz.mentalmind.ui.purchase
 
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_tariffs.*
 import kz.mentalmind.MainActivity
@@ -29,6 +32,33 @@ class TrialFragment : Fragment() {
         close.setOnClickListener {
             (activity as? MainActivity)?.onBackPressed()
         }
+
+        val textShader1 = LinearGradient(
+            0F,
+            0F,
+            0F,
+            20F,
+            intArrayOf(
+                ContextCompat.getColor(requireContext(), R.color.gr1),
+                ContextCompat.getColor(requireContext(), R.color.gr2)
+            ),
+            floatArrayOf(0F, 1F),
+            Shader.TileMode.CLAMP
+        )
+        val textShader2 = LinearGradient(
+            0F,
+            0F,
+            0F,
+            20F,
+            intArrayOf(
+                ContextCompat.getColor(requireContext(), R.color.gr3),
+                ContextCompat.getColor(requireContext(), R.color.gr4)
+            ),
+            floatArrayOf(0F, 1F),
+            Shader.TileMode.CLAMP
+        )
+        btnSub1.paint.shader = textShader1
+        btnSub2.paint.shader = textShader2
     }
 
     companion object {
