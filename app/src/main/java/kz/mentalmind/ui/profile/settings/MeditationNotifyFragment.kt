a3c87479c2cbffe32e2ff756bf9d0f6737c9e933
00049508 <7>
2020-11-22 21:01:30 +0600

    notify set

--- app/src/main/java/kz/mentalmind/ui/profile/settings/MeditationNotifyFragment.kt
+++ app/src/main/java/kz/mentalmind/ui/profile/settings/MeditationNotifyFragment.kt
@@ -1,12 +1,11 @@
 package kz.mentalmind.ui.profile.settings
 
-import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.akexorcist.snaptimepicker.SnapTimePickerDialog
+import kotlinx.android.synthetic.main.fragment_meditation_notify.*
 import kz.mentalmind.R
 
 
@@ -21,9 +20,9 @@ class MeditationNotifyFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        SnapTimePickerDialog.Builder().apply {
-            setThemeColor(R.color.colorAccent)
-            setTitleColor(R.color.white)
-        }.build().show(childFragmentManager, MeditationNotifyFragment::class.simpleName)
+        timePicker.setIs24HourView(true)
+        bg_weekend.setOnCheckedChangeListener { _, checkedId ->
+
+        }
     }
 }
